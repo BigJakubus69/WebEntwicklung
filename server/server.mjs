4751c913cb97a12e5fa9ccfdfe272b5ebadc1eb0
michaelmuradov <mcmr0755@hochschule-trier.de>
2026-02-21 15:02:44 +0100

    Port geändert

--- server/server.mjs
+++ server/server.mjs
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/kinoverwaltung';
 
 // Middleware
